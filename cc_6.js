cf56be84b19ff3fbe9a4bb0f35c7a5380b1cc197
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-09 21:46:44 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -2,16 +2,25 @@
 
 function calculateProfit(costPrice, sellingPrice, unitsSold) {
     return ((sellingPrice - costPrice) * unitsSold)
-} ;
+} ;                                                                 // Creation of function to calculate profits
 
 // Test data 1
-costPrice = 20
-sellingPrice = 30
-unitsSold = 100
-console.log(`Total Profit: $${calculateProfit(costPrice, sellingPrice, unitsSold)}`)        // Output: Total Profit: $1000
+console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`)       // Output: Total Profit: $1000
 
 // Test data 2
-costPrice = 50
-sellingPrice = 70
-unitsSold = 200
-console.log(`Total Profit: $${calculateProfit(costPrice, sellingPrice, unitsSold)}`)        // Output: Total Profit: $4000
+console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`)       // Output: Total Profit: $4000
+
+
+// Task 2 - Sales Tax Computation
+
+function calculateSalesTax (amount, taxRate) {
+    return amount * taxRate
+} ;                                                                 // Creation of function to calculate profits
+
+// Test data 1 - 7% sales tax
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`)          // Output: Sales Tax: $7
+
+// Test data 1 - 10% sales tax
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)           // Output: Sales Tax: $50
+
+
